e30a2310f1ed61f2c880ed571b88718bbfef0e3c
Robert Geyer <Robert Geyer>
2017-03-09 08:29:18 -0500

    added '5' to library for deadmau5 word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var wordLib = ["skrillex", "diplo", "galantis", "lido", "deadmau5", "getter", "zedd", "vindata", "tiesto", "flume", "branchez", "avicii", "hardwell", "machineheart"];
 
 //available letter
-var availableLetters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+var availableLetters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,5";
 
 //empty array for guessed letters
 var guesses = [];
